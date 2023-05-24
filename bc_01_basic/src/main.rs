720dcb30a1743219f64ecd9dd997d536362c963a
koory1st <32436334@qq.com>
2023-05-24 20:31:19 +0800

    feat: finish basic

--- bc_01_basic/src/main.rs
+++ bc_01_basic/src/main.rs
@@ -1,12 +1,68 @@
+use std::{
+    error::Error,
+    time::{SystemTime, UNIX_EPOCH},
+};
+
+use sha256::digest;
 fn main() {
+    let mut block_chain = BlockChain::new();
+
+    block_chain.add_block("1st");
+    block_chain.add_block("2nd");
+
+    dbg!(block_chain);
+
     println!("Hello, world!");
 }
 
+#[derive(Debug)]
 struct Block {
-    timestamp: i64,
+    timestamp: u64,
     data: String,
     prev_block_hash: String,
     hash: String,
 }
 
+impl Block {
+    fn new(data: &str, prev_block_hash: &str) -> Block {
+        let timestamp = SystemTime::now()
+            .duration_since(UNIX_EPOCH)
+            .unwrap()
+            .as_secs();
 
+        let hash = format!(
+            "{}{}{}",
+            prev_block_hash,
+            data,
+            format!("{:010}", timestamp)
+        );
+
+        let hash = digest(hash);
+        Block {
+            timestamp,
+            data: data.to_owned(),
+            prev_block_hash: prev_block_hash.to_owned(),
+            hash,
+        }
+    }
+}
+
+#[derive(Debug)]
+struct BlockChain {
+    blocks: Vec<Block>,
+}
+
+impl BlockChain {
+    fn new() -> BlockChain {
+        BlockChain {
+            blocks: vec![Block::new("Genesis Block", "")],
+        }
+    }
+    fn add_block(&mut self, data: &str) -> Result<(), Box<dyn Error>> {
+        let prev_block: &Block = self.blocks.last().expect("No Prev Block!");
+        let new_block = Block::new(data, prev_block.hash.as_ref());
+
+        self.blocks.push(new_block);
+        Ok(())
+    }
+}
